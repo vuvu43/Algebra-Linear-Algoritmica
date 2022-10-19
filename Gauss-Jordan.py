a044a3d38d91a69fa831e003903efe6d705e81fa
Arthur Pinheiro <114116008+vuvu43@users.noreply.github.com>
2022-10-18 21:53:18 -0300

    Update Gauss-Jordan.py

--- Gauss-Jordan.py
+++ Gauss-Jordan.py
@@ -14,76 +14,76 @@ def divide_linha(M, i, k):
   
   
   def subtrai_linha(M, i, j, k=1):
-  M[i, :] = M[i, :] - k*M[j, :]
+    M[i, :] = M[i, :] - k*M[j, :]
 
   return M
   
   
   def acha_pivo(M, i):
-  for j, linha in enumerate(M):
-    for coluna, valor in enumerate(linha):
-      if valor != 0:
-        if coluna == i:
-          return j
-        break
-  return 
+    for j, linha in enumerate(M):
+      for coluna, valor in enumerate(linha):
+        if valor != 0:
+          if coluna == i:
+            return j
+          break
+    return 
   
   
   def ajeita_matriz(M):
-  """
-  Função para colocar a Matriz M na forma correta do Gauus-Jordan
-  """
-  ja_foi = -1
-  for col in range(len(M[0]) - 1):
+    """
+    Função para colocar a Matriz M na forma correta do Gauus-Jordan
+    """
+    ja_foi = -1
+    for col in range(len(M[0]) - 1):
 
-    if col < ja_foi: continue
+      if col < ja_foi: continue
 
-    for lin in range(len(M)):
-      if M[lin][col] == 0:
-        num = acha_pivo(M, col)
+      for lin in range(len(M)):
+        if M[lin][col] == 0:
+          num = acha_pivo(M, col)
 
-        if num is not None:
-          M = troca_linha(M, num, lin)
-          ja_foi = col
-          break
+          if num is not None:
+            M = troca_linha(M, num, lin)
+            ja_foi = col
+            break
 
-  return M
+    return M
   
   
   def gauss_jordan(A, b):
-  A = np.column_stack((A, b))
-  A = ajeita_matriz(A)
+    A = np.column_stack((A, b))
+    A = ajeita_matriz(A)
 
-  for coluna in range(len(A[0])):
-    linha_pivo = acha_pivo(A, coluna)
+    for coluna in range(len(A[0])):
+      linha_pivo = acha_pivo(A, coluna)
 
-    if linha_pivo is not None:
-      k = A[linha_pivo][coluna]
-      A = divide_linha(A, linha_pivo, k)
+      if linha_pivo is not None:
+        k = A[linha_pivo][coluna]
+        A = divide_linha(A, linha_pivo, k)
 
-      for chave, linha0 in enumerate(A[:, coluna]):
-        if linha0 == 0:
-          linha_aux = chave
-          break
-        else:
-          linha_aux = linha_pivo
+        for chave, linha0 in enumerate(A[:, coluna]):
+          if linha0 == 0:
+            linha_aux = chave
+            break
+          else:
+            linha_aux = linha_pivo
 
-      for indice, linha in enumerate(A):
-        if indice == linha_pivo:
-          continue
+        for indice, linha in enumerate(A):
+          if indice == linha_pivo:
+            continue
         
-        A = subtrai_linha(A, indice, linha_pivo, A[indice][coluna])
+          A = subtrai_linha(A, indice, linha_pivo, A[indice][coluna])
         
-  #Nesse ponto a matriz já está pronta, você pode modificar o código
-  # para imprimi-la se quiser
+    #Nesse ponto a matriz já está pronta, você pode modificar o código
+    #para imprimi-la se quiser
   
-  tam = len(A)
-  for i in range(tam):
-      if A[i, i] == 0:
-        for j in range(i + 1, len(A[0]) - 1):
-          if  A[i, j] != 0: 
-            return "O Sistema possui infinitas soluções"
+    tam = len(A)
+    for i in range(tam):
+        if A[i, i] == 0:
+          for j in range(i + 1, len(A[0]) - 1):
+            if  A[i, j] != 0: 
+              return "O Sistema possui infinitas soluções"
 
-        return "O Sistema não possui solução"
+          return "O Sistema não possui solução"
 
-  return A
+    return A
